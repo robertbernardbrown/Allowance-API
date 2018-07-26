5f401f490f0424c9ddaaac647d5767028e3ef0f6
Bob Brown <robertbernardbrown@gmail.com>
2018-07-26 16:02:58 -0400

    adds a test for POST on budget route

--- test/test.js
+++ test/test.js
@@ -5,8 +5,9 @@ const server = require('../server');
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('Users model', () => {
+describe('API tests', () => {
 
+  // before hook to drop and make tables before tests run
   before((done) => {
     db.sequelize.sync({force : true})
       .then(() => {
@@ -17,39 +18,42 @@ describe('Users model', () => {
       });
   });
 
-  describe('GET user', () => {
-    it('it should GET a user based on their Id', (done) => {
-        chai.request(server)
-            .get('/api/users/1')
-            .end((err, res) => {
-              res.should.have.status(404);
-              res.body.should.be.an('object');
-              res.body.message.should.include("We can\'t find your information!");
-              done();
-            });
-    });
-  });
+  //========TEST SUITE ONE = USER MODEL========
+  describe('User model', () => {
 
-  describe('POST user', () => {
-    it('it should POST a new user to the /register route', (done) => {
-    let userInfo = {
-        userName: "Billybob",
-        userEmail: "billybob@gmail.com",
-        userPassword: "billybob123"
-    }
-    chai.request(server)
-        .post('/api/register')
-        .send(userInfo)
-        .end((err, res) => {
-          res.should.have.status(201);
-          res.body.should.be.a('object');
-          res.body.should.have.property('message');
-          res.body.message.should.include('User created!');
-          done();
-        });
+    describe('GET user', () => {
+      it('it should GET a user based on their Id', (done) => {
+          chai.request(server)
+              .get('/api/users/1')
+              .end((err, res) => {
+                res.should.have.status(404);
+                res.body.should.be.an('object');
+                res.body.message.should.include("We can\'t find your information!");
+                done();
+              });
+      });
     });
 
-    it('it should not POST a new user with incomplete data on /register route', (done) => {
+    describe('POST user', () => {
+      it('it should POST a new user to the /register route', (done) => {
+      let userInfo = {
+          userName: "Billybob",
+          userEmail: "billybob@gmail.com",
+          userPassword: "billybob123"
+      }
+      chai.request(server)
+          .post('/api/register')
+          .send(userInfo)
+          .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message');
+            res.body.message.should.include('User created!');
+            done();
+          });
+      });
+
+      it('it should not POST a new user with incomplete data on /register route', (done) => {
       let userInfo = {
           userEmail: "billybob123@gmail.com",
           userPassword: "billybob123"
@@ -73,24 +77,68 @@ describe('Users model', () => {
       });
 
       it('it should not POST a new user with duplicate email as another user on /register route', (done) => {
-        let userInfo = {
-          userName: "Billybob2",
-          userEmail: "billybob@gmail.com",
-          userPassword: "billybob123"
-        }
-        chai.request(server)
-            .post('/api/register')
-            .send(userInfo)
-            .end((err, res) => {
-              res.should.have.status(400);
-              res.body.should.be.a('object');
-              res.body.should.have.property('message');
-              res.body.message.should.include("User exists already with that email!");
-              done();
-            });
-        });
+      let userInfo = {
+        userName: "Billybob2",
+        userEmail: "billybob@gmail.com",
+        userPassword: "billybob123"
+      }
+      chai.request(server)
+          .post('/api/register')
+          .send(userInfo)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message');
+            res.body.message.should.include("User exists already with that email!");
+            done();
+          });
+      });
+    });
   });
 
+  //========TEST SUITE TWO = Budget MODEL========
+  describe('Budget model', () => {
+
+    describe('GET budget', () => {
+      it('it should return an confirmation that no budgets exist if no budgets exist', (done) => {
+        chai.request(server)
+            .get('/api/budgets/1')
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.an('object');
+              res.body.should.have.property('message');
+              res.body.message.should.include("You have no budgets to display yet");
+              done();
+            });
+      });
+    });
+
+    describe('POST budget', () => {
+      it('it should return an error message if any piece of budget data is missing', (done) => {
+      const budget = {
+        budget: 200,
+        userId: 1
+      }
+      chai.request(server)
+        .post('/api/budgets/1')
+        .send(budget)
+        .end((err, res) => {
+          res.should.have.status(500);
+          res.body.should.be.an('object');
+          res.body.should.have.property('message');
+          res.body.message.should.include("Something went wrong posting that budget!");
+          res.body.should.have.property('error');
+          res.body.error.should.be.an('object');
+          res.body.error.name.should.include('SequelizeValidationError');
+          res.body.error.errors[0].message.should.include('Budget.budgetMonth cannot be null');
+          done();
+        });
+      });
+    });
+
+  }); 
+
+  //after hook to drop and make tables before tests run
   after((done) => {
     db.sequelize.sync({force : true})
       .then(() => {
