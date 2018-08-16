7ff028a72fd1583ae202f55beb7d0f8e5fe009f9
Bob Brown <robertbernardbrown@gmail.com>
2018-08-16 12:32:52 -0400

    adds hashing to passwords in register path

--- api/routes/register.js
+++ api/routes/register.js
@@ -1,32 +1,46 @@
 const express = require("express");
 const router  = express.Router();
 const db      = require("../models");
+const bcrypt = require("bcryptjs");
 
 //Register a new user if they don't exist already
 router.post("/", (req, res, next)=>{
-    const userInfo = {
-        userName: req.body.userName,
-        userEmail: req.body.userEmail,
-        userPassword: req.body.userPassword
-    }
     db.User
-    .findOrCreate({where: {userEmail: req.body.userEmail}, defaults: userInfo})
-    .spread((user, created) => {
-        if (created) {
-            res.status(201).json({
-                message: "User created!"
-            })
-        } else {
-            res.status(400).json({
+    .findOne({where: {userEmail: req.body.userEmail}})
+    .then((user) => {
+        if (user) {
+            res.status(409).json({
                 message: "User exists already with that email!"
             })
+        } else {
+            bcrypt.hash(req.body.userPassword, 10, (err, hash) => {
+                if (err) {
+                    return res.status(500).json({
+                        error:err
+                    });
+                } else {
+                    const userInfo = {
+                        userName: req.body.userName,
+                        userEmail: req.body.userEmail,
+                        userPassword: hash
+                    }
+                    db.User
+                    .create(userInfo)
+                    .then((result) => {
+                        res.status(201).json({
+                            message: "User created!"
+                        })
+                    })
+                    .catch(err=>{
+                        res.status(500).json({
+                            message: "There was an error processing your information",
+                            error: err
+                        })
+                    })
+                }
+            });
+            
         }
-    })
-    .catch(err=>{
-        res.status(500).json({
-            message: "There was an error processing your information",
-            error: err
-        })
     })
 });
 
