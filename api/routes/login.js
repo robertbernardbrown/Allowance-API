c870ae138bf8fd7083f4e173c27bc7418dbeb3b6
Bob Brown <robertbernardbrown@gmail.com>
2018-08-17 12:08:19 -0400

    adds bcrypt compare method to login function to check passwords

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,24 +1,36 @@
 const express = require("express");
 const router  = express.Router();
 const db      = require("../models");
+const bcrypt  = require("bcryptjs");
 
 //Login a valid user
 router.post("/", (req, res, next)=>{
-    const loginInfo = {
-        userName: req.body.userName,
-        userPassword: req.body.password
-    }
-    db.User.findOne({ where: {userName: req.body.userName} })
+    db.User.findOne({ where: {userEmail: req.body.userEmail} })
     .then(result=>{
         console.log(result);
-        res.status(200).json({
-            message: "Login success",
-            result: result
+        if (!result) {
+            return res.status(400).json({
+                message: "Auth failed"
+            });
+        }
+        bcrypt.compare(req.body.userPassword, result.dataValues.userPassword, (err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    message: "Auth failed"
+                });
+            }
+            if (data) {
+                return res.status(200).json({
+                    message: "Auth successful"
+                });
+            }
+            res.status(400).json({
+                message: "Auth failed"
+            });
         })
     })
     .catch(err=>{
-        console.log(err);
-        res.status(404).json({
+        res.status(500).json({
             message: "Something went wrong",
             error: err
         })
