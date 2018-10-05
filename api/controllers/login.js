9eceb73a4aa85a5c2614558f1c8d0a839afe0554
Bob Brown <robertbernardbrown@gmail.com>
2018-10-05 14:39:11 -0400

    adds more error handling to login form

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -3,7 +3,50 @@ const bcrypt  = require("bcryptjs");
 const jwt     = require("jsonwebtoken");
 require("dotenv").config();
 
+  /**
+   * Validate the login form
+   *
+   * @param {object} payload - the HTTP body message
+   * @returns {object} The result of validation. Object contains a boolean validation result,
+   *                   errors tips, and a global message for the whole form.
+   */
+  function validateLoginForm(payload) {
+    const errors = {};
+    let isFormValid = true;
+    let message = '';
+  
+    if (!payload || typeof payload.userEmail !== 'string' || payload.userEmail.trim().length === 0) {
+      isFormValid = false;
+      errors.userEmail = 'Please provide your email address.';
+    }
+  
+    if (!payload || typeof payload.userPassword !== 'string' || payload.userPassword.trim().length === 0) {
+      isFormValid = false;
+      errors.userPassword = 'Please provide your password.';
+    }
+  
+    if (!isFormValid) {
+      message = 'Check the form for errors.';
+    }
+  
+    return {
+      success: isFormValid,
+      message,
+      errors
+    };
+}
+
 exports.user_login = (req, res, next)=>{
+
+    const validationResult = validateLoginForm(req.body);
+    if (!validationResult.success) {
+        return res.status(400).json({
+            success: false,
+            message: validationResult.message,
+            errors: validationResult.errors
+        });
+    }
+
     db.User.findOne({ where: {userEmail: req.body.userEmail} })
     .then(result=>{
         if (!result) {
